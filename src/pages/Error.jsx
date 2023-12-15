edbeec8231ec4543819cf025c1801b4818a0a154
patoche62500 <b.grebert62@gmail.com>
2023-12-15 16:11:37 +0100

    add page error & css

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,16 +1,20 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, Link } from "react-router-dom";
+import Header from "../components/header/index";
+import Footer from "../components/footer/index";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <>
+      <Header />
+      <section className="error">
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <Link to={"/"}>Retourner sur la page d’accueil</Link>
+      </section>
+      <Footer />
+    </>
   );
 }
